e40a9f26ff164fbb7ca142d4dcacf2672ca10631
Tatsuya Tsuyama <tatsuyatsuyama@gmail.com>
2019-02-12 21:45:47 +0900

    [Modify]STATION-33 API実装  ユーザ情報取得、情報更新API

--- fuel/app/modules/api/classes/controller/user.php
+++ fuel/app/modules/api/classes/controller/user.php
@@ -103,36 +103,76 @@ class Controller_User extends Controller_Base
     }
   }
 
-//  public function get_my_info()
-//  {
-//    try {
-//      if (!\Auth::check()) {
-//        $this->failed();
-//        $this->error = [
-//          E::UNAUTHNTICATED,
-//          'need to login.'
-//        ];
-//        return;
-//      }
-//      $id = \Auth::get_user_id()[1];
-//
-//      if (!empty($email)) {
-//        $params['email'] = $email;
-//      }
-//      if (!empty($nickname)) {
-//        $params['email'] = $nickname;
-//      }
-//      \Auth::update_user($params
-//      );
-//      unset($this->body['data']);
-//      $this->success();
-//    } catch (\Exception $e) {
-//      $this->failed();
-//      $this->error = [
-//        E::INVALID_REQUEST,
-//        $e->getMessage()
-//      ];
-//    }
-//  }
+  public function get_my_info()
+  {
+    try {
+      if (!$user = \Auth_User::get_user()) {
+        $this->failed();
+        $this->error = [
+          E::UNAUTHNTICATED,
+          'no user found.'
+        ];
+        return;
+      }
+      $result = $user->to_array();
+      $result['nickname'] = $user->nickname;
+      $this->success();
+      $this->data = $result;
+    } catch (\Exception $e) {
+      $this->failed();
+      $this->error = [
+        E::INVALID_REQUEST,
+        $e->getMessage()
+      ];
+    }
+  }
+
+  public function get_one()
+  {
+    $id = \Input::get('id');
+    try {
+      if (!$user = \Auth_User::find($id)) {
+        $this->not_found();
+      }
+      $user->metadata;
+      $this->success();
+      $this->data = $user;
+
+
+    } catch (\Exception $e) {
+      $this->failed();
+      $this->error = [
+        E::INVALID_REQUEST,
+        $e->getMessage()
+      ];
+    }
+  }
+
+  public function patch_change_password()
+  {
+    try {
+      if (!\Auth::check()) {
+        $this->failed();
+        $this->error = [
+          E::UNAUTHNTICATED,
+          'need to login.'
+        ];
+        return;
+      }
+      $old_password = \Input::patch('old_password');
+      $new_password = \Input::patch('new_password');
+
+      \Auth::change_password($old_password, $new_password
+      );
+      unset($this->body['data']);
+      $this->success();
+    } catch (\Exception $e) {
+      $this->failed();
+      $this->error = [
+        E::INVALID_REQUEST,
+        $e->getMessage()
+      ];
+    }
+  }
 }
 
