bca52441c72479f7ba2f2a7ff436497a5cf70383
Tatsuya Tsuyama <tatsuyatsuyama@gmail.com>
2019-02-19 21:59:16 +0900

    [Fix]駅検索APIレスポンス修正

--- fuel/app/modules/api/classes/controller/contribution.php
+++ fuel/app/modules/api/classes/controller/contribution.php
@@ -81,4 +81,53 @@ class Controller_Contribution extends Controller_Base
       $this->body['errorlog'] = $e->getMessage();
     }
   }
+
+  public function post_contribute(){
+    try{
+      $route_id = \Input::post('route_id');
+      $station_id = \Input::post('station_id');
+      $site_id = \Input::post('site_id');
+      $facility_id = \Input::post('facility_id');
+      $overview = \Input::post('overview');
+      $remarks = \Input::post('remarks');
+      $config = array(
+        'path' =>  DOCROOT . 'contents/', //保存先のパス
+        'randomize' => true, //ファイル名をランダム生成
+        //'new_name' => $data['file_name'],
+        'auto_rename' => true,
+        'ext_whitelist' => array('jpg', 'jpeg', 'png'),
+        'max_size' => 0,//制限なし
+        'suffix' => '_' . date( "Ymd" ), //ファイル名の最後に文字列を付与
+        //'auto_rename' => true, //ファイル名が重複した場合、連番を付与
+        'auto_process' => false
+      );
+      mb_convert_variables('UTF-8', 'UTF-8', $config);
+      \Upload::process($config);
+      if (\Upload::is_valid()) {
+        \Upload::save();
+        $file = \Upload::get_files();
+
+        // 正常保存された場合、アップロードファイル情報を取得
+        if ($file) {
+          $thumbnail_before = $file['name'];
+          $saved_as = $file['saved_as'];
+          $saved_to = $file['saved_to'];
+          $mimetype = $file['mimetype'];
+          $extension = $file['extension'];
+          $size = $file['size'];
+          $data = \Input::post();
+        }
+      }
+      $thumbnail_before = \Input::post('thumbnail_before');
+
+
+    }catch(\Exception $e){
+      $this->failed();
+      $this->error = [
+        E::SERVER_ERROR,
+        '投稿処理に失敗しました。'
+      ];
+      $this->body['errorlog'] = $e->getMessage();
+    }
+  }
 }
