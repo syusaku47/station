01c58b212753dea37e1d94b6b8e54019cb6a69c7
Tatsuya Tsuyama <tatsuyatsuyama@gmail.com>
2019-03-11 20:59:37 +0900

    [Modify]画像アップロード処理

--- fuel/app/modules/api/classes/controller/admin/contribution.php
+++ fuel/app/modules/api/classes/controller/admin/contribution.php
@@ -402,7 +402,7 @@ class Controller_Admin_Contribution extends Controller_Base
           'randomize' => true, //ファイル名をランダム生成
           //'new_name' => $data['file_name'],
           'auto_rename' => true,
-          'ext_whitelist' => array('jpg', 'jpeg', 'png'),
+          //'ext_whitelist' => array('jpg', 'jpeg', 'png'),
           'max_size' => 0,//制限なし
           'suffix' => '_' . date("Ymd"), //ファイル名の最後に文字列を付与
           //'auto_rename' => true, //ファイル名が重複した場合、連番を付与
@@ -410,7 +410,7 @@ class Controller_Admin_Contribution extends Controller_Base
         );
         mb_convert_variables('UTF-8', 'UTF-8', $config);
         \Upload::process($config);
-        //if (\Upload::is_valid()) {
+        if (\Upload::is_valid()) {
           \Upload::save();
           $files = \Upload::get_files();
 
@@ -449,13 +449,13 @@ class Controller_Admin_Contribution extends Controller_Base
               'サムネイルの保存に失敗しました'
             ];
           }
-//        } else {
-//          $this->failed();
-//          $this->error = [
-//            E::SERVER_ERROR,
-//            '不正なファイルです'
-//          ];
-//        }
+        } else {
+          $this->failed();
+          $this->error = [
+            E::SERVER_ERROR,
+            '不正なファイルです'
+          ];
+        }
       }
 
       $needs_send_mail = $contribute->repairer_id != $repairer_id ? true : false;
